90d24a4f0cc4f5870ab1204b3605936fe0742d6b
Matt Comins <mattcomins@gmail.com>
2024-09-04 10:28:22 -0500

    update routes: include health check

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const { Nuxt } = require('nuxt');
 const app = express();
 const config = require('../nuxt.config.js');
 
+app.use('/api/health', require('./routes/health'));
+
 const nuxt = new Nuxt(config);
 app.use((req, res, next) => {
   nuxt.ready().then(() => nuxt.render(req, res, next));
